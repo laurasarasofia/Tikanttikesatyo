b652dcd3db40efbc7ec608898376198dbe01534d
laurasarasofia <laurasarasofiaa@gmail.com>
2023-02-13 16:09:34 +0200

    parempi ulkonäkö

--- app.js
+++ app.js
@@ -6,31 +6,20 @@ switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
-
     const className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
+    this.textContent = className === "light-theme" ? "Dark" : "Light";
 
-    console.log('current class name: ' + className);
+    console.log(`Current class name: ${className}`);
 });
 
 function openTab(evt, id) {
-    var i, tabcontent, tablinks;
+    const tabcontent = document.getElementsByClassName("tabcontent");
+    Array.from(tabcontent).forEach(el => el.style.display = "none");
 
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
+    const tablinks = document.getElementsByClassName("tablinks");
+    Array.from(tablinks).forEach(el => {
+        el.className = el.className.replace(" active", "");
+    });
 
     document.getElementById(id).style.display = "block";
     evt.currentTarget.className += " active";
@@ -53,17 +42,21 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  Array.from(slides).forEach(el => el.style.display = "none");
+  Array.from(dots).forEach(el => {
+    el.className = el.className.replace(" active", "");
+  });
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
